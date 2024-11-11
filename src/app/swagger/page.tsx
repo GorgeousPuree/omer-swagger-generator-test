4049a8c8a554d8a6cae10fb753ba38898fe8b94a
GorgeousPuree <eydnep2000@gmail.com>
2024-11-11 12:50:00 +0100

    Fix typo

--- src/app/swagger/page.tsx
+++ src/app/swagger/page.tsx
@@ -1,10 +1,10 @@
 import generateOpenApiSpec from "@omer-x/next-openapi-json-generator";
 import ReactSwagger from "@/components/ReactSwagger";
-import { LicenceDTO } from "@/models/license";
+import { LicenseDTO } from "@/models/license";
 
 const Page = async () => {
   const spec = await generateOpenApiSpec({
-    LicenceDTO,
+    LicenseDTO,
   });
   return <ReactSwagger spec={spec} />;
 };
